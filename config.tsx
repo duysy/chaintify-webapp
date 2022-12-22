a3aed7aed1f33159212190caabc4f2bafb1cbde8
Duy Sy Nguyen <duyduysysy@gmail.com>
2022-12-22 10:59:17 +0700

    update contract

--- config.tsx
+++ config.tsx
@@ -1,5 +1,5 @@
 export default {
   // baseMedia: "chaintify:8000/music/api/public/upload/?path=",
   baseMedia: "http://127.0.0.1:8000/music/api/public/upload/?path=",
-  chaintifyContract: "0xeb7d600371C4a7790f822B02b7Bfc05900884EAb",
+  chaintifyContract: "0x7e963c0822E8ef6b29CBD59E50b2F620e4C809e3",
 };
