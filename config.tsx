735d436b995b6b44ba6b605f33dfc49ad841784b
Duy Sy Nguyen <duyduysysy@gmail.com>
2022-12-25 22:28:02 +0700

    update config

--- config.tsx
+++ config.tsx
@@ -4,7 +4,7 @@ export default {
   // IMAGE_URL: "http://server:8000/media/",
   // MUSIC_URL: "http://127.0.0.1:8000/media/",
   MUSIC_URL: "http://108.61.245.173:8000/media/",
-  API_URL: "http://108.61.245.173/music/api/",
+  API_URL: "http://108.61.245.173:8000/music/api/",
   CHAINTIFY_CONTRACT: "0xc60d2F5F02ee4d4BaCbfDAae3438bB7D087936d4",
   ALCHEMY_KEY: "Gnp3tbW0Wp0tJpdhVz1ZRtB9bqgYG03O",
   EXPLORER: "https://mumbai.polygonscan.com/tx",
