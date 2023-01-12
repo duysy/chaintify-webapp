e028a9dfe06e6949bc2134436c41373bfe86853d
Duy Sy Nguyen <duyduysysy@gmail.com>
2023-01-12 22:13:21 +0700

    update ip config

--- config.tsx
+++ config.tsx
@@ -7,9 +7,9 @@ export default {
   // IMAGE_URL: "http://server:8000/music/api/public/upload/?path=",
   // MUSIC_URL: "http://127.0.0.1:8000/media/",
 
-  API_URL: "http://103.39.125.95/music/api/",
+  API_URL: "http://103.39.125.95:8000/music/api/",
   IMAGE_URL: "http://server:8000/music/api/public/upload/?path=",
-  MUSIC_URL: "http://103.39.125.95/media/",
+  MUSIC_URL: "http://103.39.125.95:8000/media/",
 
   CHAINTIFY_CONTRACT: "0xAC9676B8dacAfe02986B35d2D045724C7D1B9F93",
   ALCHEMY_KEY: "Gnp3tbW0Wp0tJpdhVz1ZRtB9bqgYG03O",
