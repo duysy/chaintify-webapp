91a199f93e00d6922e588af9b9dbe43a809de4ba
Duy Sy Nguyen <duyduysysy@gmail.com>
2023-03-24 20:38:26 +0700

    update

--- config.tsx
+++ config.tsx
@@ -12,7 +12,7 @@ export default {
   // MUSIC_URL: "http://127.0.0.1:8000/media/",
 
   API_URL: "https://api.chaintify.space/music/api/",
-  IMAGE_URL: "http://nginx:8000/music/api/public/upload/?path=",
+  IMAGE_URL: "https://api.chaintify.space/music/api/public/upload/?path=",
   MUSIC_URL: "https://api.chaintify.space/media/",
 
   CHAINTIFY_CONTRACT: "0x4C4B27C6b1867F5d71F974c0e02446fC267C4a2a",
